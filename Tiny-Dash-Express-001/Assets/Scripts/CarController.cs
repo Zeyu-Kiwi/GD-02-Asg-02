cce099d6915ce8ba0bde761482002339502bcea1
Zeyu-Kiwi <zhouzeyuabc@gmail.com>
2026-03-23 16:39:23 +0800

    Code Commit 02
    - basic car controller with acceleration, deceleration, steer, brake, wheel animation

--- Tiny-Dash-Express-001/Assets/Scripts/CarController.cs
+++ Tiny-Dash-Express-001/Assets/Scripts/CarController.cs
@@ -1,19 +1,110 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class CarController : MonoBehaviour
 {
-    public float speed = 10f;
-    public float turnSpeed = 50f;
+	public enum Axel
+	{
+		Front,
+		Rear
+	}
+
+	[Serializable]
+	public struct Wheel
+	{
+		public GameObject wheelModel;
+		public WheelCollider wheelCollider;
+		public Axel axel;
+	}
+
+	public float maxAcceleration = 30.0f;
+	public float brakeAcceleration = 50.0f;
+
+	public float turnSansitivity = 1.0f;
+	public float maxSteerAngle = 30.0f;
+
+	public Vector3 _centerOfMass;
+
+	public List<Wheel> wheels;
+
+	float moveInput;
+	float steerInput;
+
+	private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+		carRb.centerOfMass = _centerOfMass;
+    }
 
     void Update()
     {
-        // Move forward/backward
-        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(0, 0, move);
-
-        // Turn left/right
-        float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        transform.Rotate(0, turn, 0);
+        GetInputs();
+		AnimateWheels();
     }
+
+    void FixedUpdate() //was LateUpdate
+    {
+		Move();
+		Steer();
+		Brake();
+    }
+
+    void GetInputs()
+	{
+		moveInput = Input.GetAxis("Vertical");
+		steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+		foreach (var wheel in wheels)
+		{
+			wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime; // AI suggested to remove Time.deltaTime
+		}	
+    }
+
+	void Steer()
+	{
+		foreach(var wheel in wheels)
+		{
+			if (wheel.axel == Axel.Front)
+			{
+				var _steerAngle = steerInput * turnSansitivity * maxSteerAngle;
+				wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+			}
+		}
+	}
+
+	void Brake()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			foreach (var wheel in wheels)
+			{
+				wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+			}
+		}
+		else
+		{
+			foreach( var wheel in wheels)
+			{
+				wheel.wheelCollider.brakeTorque = 0;
+			}
+		}
+	}
+
+	void AnimateWheels()
+	{
+		foreach (var wheel in wheels)
+		{
+			Quaternion rot;
+			Vector3 pos;
+			wheel.wheelCollider.GetWorldPose(out pos, out rot);
+			wheel.wheelModel.transform.position = pos;
+			wheel.wheelModel.transform.rotation = rot;
+		}
+	}
 }
