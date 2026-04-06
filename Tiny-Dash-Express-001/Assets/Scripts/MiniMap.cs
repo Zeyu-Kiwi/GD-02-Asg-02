58443633d0fdaf56a4d0cef88e545363c55e490c
Zeyu-Kiwi <zhouzeyuabc@gmail.com>
2026-04-06 16:05:27 +0800

    UI 09
    - added mini map

---
+++ Tiny-Dash-Express-001/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    public Transform player;
+    public Transform mark;
+
+    private void LateUpdate()
+    {
+        MiniMapCamFollow();
+    }
+
+    private void MiniMapCamFollow()
+    {
+        Vector3 newposition = player.position;
+        newposition.y = transform.position.y;
+        transform.position = newposition;
+
+        //rotate cam with truck
+        //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        mark.transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y - 90f, 0f);
+    }
+}
