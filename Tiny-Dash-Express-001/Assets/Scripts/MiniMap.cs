2d1a06a0cfc34d126170e4af6e9e986e4a7e477b
Zeyu-Kiwi <zhouzeyuabc@gmail.com>
2026-04-06 16:15:00 +0800

    UI 02
    - edit a little to the minimap script

--- Tiny-Dash-Express-001/Assets/Scripts/MiniMap.cs
+++ Tiny-Dash-Express-001/Assets/Scripts/MiniMap.cs
@@ -16,6 +16,11 @@ public class MiniMap : MonoBehaviour
         newposition.y = transform.position.y;
         transform.position = newposition;
 
+        newposition.y = mark.transform.position.y;
+        mark.transform.position = newposition;
+
+
+
         //rotate cam with truck
         //transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
         mark.transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y - 90f, 0f);
